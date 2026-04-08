1e50e6bf9248d1d6f0c7d321f4361ec0c939e7b9
Ewerton Madruga <emadruga@gmail.com>
2026-04-08 14:56:44 -0300

    troca de senha usuario Kaua Franca.

---
+++ server/past_migration_scripts/migration_reset_pw_20260408.sql
@@ -0,0 +1,7 @@
+-- Reset de senha
+-- Gerado em: 2026-04-08 14:49:47 por manage_users.py
+-- Revise cuidadosamente antes de aplicar.
+--
+-- user_id=90  username=França  nome=Kauã Alves da Silva de França
+
+UPDATE users SET password_hash = '$2b$12$UX/VBsnYQ1377mN1El2rtOPvcG7XnLG/s/pD.Raa5tJmqlRpiKb8m' WHERE user_id = 90;
