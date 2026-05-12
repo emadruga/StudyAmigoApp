902ae067112da0c2b08625373cef80202549838b
Ewerton Madruga <emadruga@gmail.com>
2026-05-12 19:53:16 -0300

    admin: update email for user_id=75 (Isabel Peixoto)

---
+++ server/past_migration_scripts/migration_update_email_20260512.sql
@@ -0,0 +1,9 @@
+-- Atualização de email
+-- Gerado em: 2026-05-12 19:46:13 por manage_users.py
+-- Revise cuidadosamente antes de aplicar.
+--
+-- user_id=75  username=belpxto  nome=Isabel da Silva Peixoto
+-- email anterior: isabeldasilvapeixoto@gmail.com
+-- email novo: isabeldasilvapeixoto973@gmail.com
+
+UPDATE users SET email = 'isabeldasilvapeixoto973@gmail.com' WHERE user_id = 75;
